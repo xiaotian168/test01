dd3c369333b970c2ac99c7a350a8b7c53e93e1d8
xiaotian168 <1134813948@qq.com>
2017-06-30 16:47:11 +0800

    查询API

--- mySql/select.c
+++ mySql/select.c
@@ -2,34 +2,52 @@
 #include <stdlib.h>
 #include <mysql.h>
 
+//查询表中数据；并将结果输出打印到屏幕
+
 int main()
 {
+    //1.创建mysql
     MYSQL *mysql = mysql_init(NULL);
-
+    //2.连接数据库；赋值结果给mysql
     mysql = mysql_real_connect(mysql, "127.0.0.1", "root", "itcast", "test", 3306, NULL, 0);
+    if(NULL == mysql)
+    {
+        printf("connect error\n");
+        return -1;
+    }
 
-    //设置编码格式
-    mysql_query(mysql, "set names utf8");
+    //3.设置编码格式    -->0表示执行成功
+    if(mysql_query(mysql, "set names utf8"))
+    {
+        printf("编码格式设置出错\n");
+        return -2;
+    }
 
-    //执行查询语句
-    mysql_query(mysql, "select * from test4");
 
-    //检索结果集
+    //4.执行查询语句
+    if(mysql_query(mysql, "select * from test4"))
+    {
+        printf("select error\n");
+        return -3;
+    }
+
+    //5.获取结果；并输出到屏幕
+    //检索结果集; 获取结果结构体
     MYSQL_RES *result = mysql_store_result(mysql);
-    //获取结果集中的字段数
+    //从结果中获取结果集中的字段数  -->分解得到结果集中单个数据信息
     unsigned int count = mysql_num_fields(result);
     printf("count = %d\n", count);
     //获取表头,字段值
-    for(int i = 0; i < count; ++i)
+    for(int i = 0; i < count; ++i)  //获取打印没个字段值
     {
         MYSQL_FIELD *field = mysql_fetch_field(result);
         printf("%s\t", field->name);
     } 
-    printf("\n");
+    printf("\n");   //换行
 
     //获取结果集中行的数据
     MYSQL_ROW row;
-    while(row = mysql_fetch_row(result))
+    while(row = mysql_fetch_row(result))    //自动读取每行数据
     {
         for(int i = 0; i<count; ++i)
         {
@@ -37,10 +55,10 @@ int main()
         }
         printf("\n");
     }
-    
 
     //释放结果集
     mysql_free_result(result);
+
     //释放连接
     mysql_close(mysql);
     return 0;
